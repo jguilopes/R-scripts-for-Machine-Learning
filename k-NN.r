5c3fad8220e98ed297dced81f5b42e4a459968ee
Jose Guilherme Lopes <lopes.jguilherme@gmail.com>
2018-08-22 08:23:45 -0300

    .

--- k-NN.r
+++ k-NN.r
@@ -3,19 +3,19 @@
 library(tidyverse)
 library(class)
 
-# Função para implementar o k-NN:
+# FunÃ§Ã£o para implementar o k-NN:
 ?knn()
 
 # Importando dataset:
 
-dataset <- read_delim("C:/Users/lopes/Dropbox/Introducao a Mineracao de Dados/Capítulo 3/3.1 - Classificacao/k-NN-dataset.csv", ";", escape_double = FALSE, locale = locale(decimal_mark = ","), trim_ws = TRUE)
+dataset <- read_delim("", ";", escape_double = FALSE, locale = locale(decimal_mark = ","), trim_ws = TRUE)
 
 # Separando dataset de treinamento e variavel com as classes:
 
 treinamento <- dataset[2:12, 2:3]
 rotulos <- dataset[2:12, 4:4]
 
-# Separando observação(exemplar) cuja classe deve ser prevista pelo algoritmo k-NN:
+# Separando observaÃ§Ã£o(exemplar) cuja classe deve ser prevista pelo algoritmo k-NN:
 
 teste <- dataset[1:1, 2:3]
 
